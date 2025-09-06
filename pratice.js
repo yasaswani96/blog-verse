91542d7ce13e00b600075c02779aa11c834317da
yasaswani96 <goruyasaswani@gmail.com>
2025-09-06 12:33:58 +0530

    learned datatypes in js

--- pratice.js
+++ pratice.js
@@ -1,1 +1,25 @@
 console.log("Hello World,Yasaswani")
+let name = "Dev Astra";
+console.log(name)
+let num = 18;
+console.log(num)
+let islogged = false;
+console.log(islogged)
+let x;
+console.log(x)
+let y = null;
+console.log(y)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+let person ={
+    name:"yasaswani",
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
